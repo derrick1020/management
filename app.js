91ba3d74068e8af7289935044f737113e456e807
Derrick Helgeson <dkmk1020@gmail.com>
2021-05-02 15:48:40 -0700

    still needs work

---
+++ app.js
@@ -0,0 +1,49 @@
+const mysql = require("mysql");
+const inq = require("inquirer");
+const table = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "102044dh!",
+  database: "company_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  exports.start();
+});
+
+exports.start = () => {
+    inq.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "choice",
+            choices: [
+                "View All Employees",
+                "Add Employee",
+                "Update Employee Role",
+                "EXIT"                
+            ]
+        }
+    ])
+    .then(function(answer) {
+      if(answer.choice === "View All Employees") {
+        view.viewAllEmployees();
+      }
+      else if(answer.choice === "Add Employee") {
+        add.addEmployee();
+      }      
+      else if(answer.choice === "Update Employee Role") {
+        update.updateRole();
+      }
+      else if(answer.choice === "EXIT") {
+        connection.end();
+        return
+      }
+    });
+    
+};
